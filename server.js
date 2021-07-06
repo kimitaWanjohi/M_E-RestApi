f68cbac11fc7f5b6ae0afbcbfb9f5f138b4c1d0b
unknown <kimitawanjohi7923@gmail.com>
2021-07-06 09:33:25 +0300

    connected to db successfully

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const env = require('dotenv')
+const mongoose = require('mongoose')
 
+env.config()
 const app = express();
 
+// Routes 
+const postRoutes = require('./routes/posts');
 
-app.post('/', (req, res)=> {
-    res.send('got a post request')
-})
+
+app.use('/posts', postRoutes)
+
+// Database
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true },
+    () => console.log('connects')
+    )
+
+
 
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
 
